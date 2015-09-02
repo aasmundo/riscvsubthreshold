09da1f842d6493ee39d765eb3f4fcb6aa0073423
Aasmund <aaskvoma@hotmail.com>
2015-09-02 17:26:42 +0200

    add instruction decode and register file module

---
+++ modules/instruction_decode.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+library work;
+use work.constants.all;
+
+
+entity instruction_decode is
+	port(
+	
+	);
+end instruction_decode;
+
+
+architecture behave of instruction_decode is
+
+begin
+	
+end behave;
