109af2ecd747ccec2c509329dbf3455837a63df5
Aasmund <aaskvoma@hotmail.com>
2015-10-22 19:17:12 +0200

    read instructions from hex file

--- modules/top_bench.vhd
+++ modules/top_bench.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_textio.all;
+use std.textio.all;
 library work;
 use work.constants.all;
 
@@ -12,6 +14,27 @@ architecture behave of top_bench is
 signal clk : std_logic := '0';
 signal nreset : std_logic := '0';
 type mem_t is array(0 to ((2**PC_WIDTH) - 1)) of std_logic_vector(31 downto 0);
+
+
+impure function ocram_ReadMemFile(FileName : STRING) return mem_t is
+  file FileHandle       : TEXT open READ_MODE is FileName;
+  variable CurrentLine  : LINE;
+  variable TempWord     : STD_LOGIC_VECTOR(31 downto 0);
+  variable Result       : mem_t    := (others => (others => 'U'));
+
+begin
+  for i in 0 to (2**PC_WIDTH) - 1 loop
+    exit when endfile(FileHandle);
+
+    readline(FileHandle, CurrentLine);
+    hread(CurrentLine, TempWord);
+    Result(i)    := TempWord;
+  end loop;
+
+  return Result;
+end function;
+
+
 signal program : mem_t;
 signal imem_we : std_logic;
 signal imem_data : std_logic_vector(31 downto 0);
@@ -34,15 +57,17 @@ design : entity work.top port map
 process(clk) 
 variable i : integer := 0;
 begin
+
+program <= ocram_ReadMemFile("C:\prosjektoppgave\riscvsubthreshold\modules\test.hex");
 	
-program(0) <= "00000000000000000011000010010111";	--ADDI rs1 = 0 rd = 1
-program(1) <= "00000000000000000011000010010111";
-program(2) <= "00000000000000000011000010010111";
-program(3) <= "00000000000000000000000000000000";
-program(4) <= "00000000000000000000000000000000";
-program(5) <= "00000000000000000000000000000000";
-program(6) <= "00000000000000000000000000000000";
-program(7) <= "00000000000000000000000000000000";
+--program(0) <= "00000010000000000000000011100111";	--ADDI rs1 = 0 rd = 1
+--program(1) <= "00000000000000000000000000000000";
+--program(2) <= "00000000000000000000000000000000";
+--program(3) <= "00000000000000000000000000000000"; --JAL
+--program(4) <= "00000000000000000000000000000000";
+--program(5) <= "00000000000000000000000000000000";
+--program(6) <= "00000000000000000000000000000000";
+--program(7) <= "00000000000000000000000000000000";
 --program(5) <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 if(clk'event and clk = '1') then
