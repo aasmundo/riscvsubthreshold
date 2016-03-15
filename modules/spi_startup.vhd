8b5e4b5cc160b5c87de41902626e93563dc0d7b1
Aasmund <aaskvoma@hotmail.com>
2016-03-15 18:20:25 +0100

    spi_startup fix

--- modules/spi_startup.vhd
+++ modules/spi_startup.vhd
@@ -31,18 +31,24 @@ end entity;
 
 
 architecture behave of spi_startup is
-type state_t is (STARTUP_WAIT, WRITE_SETTINGS_1, WRITE_INSTR_ADDRESS_START, WRITE_INSTR_ADDRESS_WAIT, 
-				 WRITE_SETTINGS_2, RECV_DATA_START, RECV_DATA_WAIT, IDLE);
+type state_t is (STARTUP_WAIT, READ_STATUS_1, READ_STATUS_2, READ_STATUS_3,
+READ_DATA_1, READ_DATA_2, READ_DATA_3, READ_DATA_4, IDLE);
+constant read_status_settings : std_logic_vector(31 downto 0) := x"00000001";
+constant read_status_seq : std_logic_vector(31 downto 0) := x"05000000";
+constant read_data_settings_1 : std_logic_vector(31 downto 0) := x"02000000";
+constant read_data_settings_2 : std_logic_vector(31 downto 0) := x"03000000";
+constant read_data_seq     : std_logic_vector(31 downto 0) := x"03000000";
 signal state, n_state : state_t;
-signal word_cnt, n_word_cnt : unsigned(10 downto 0);
+signal word_cnt, n_word_cnt, word_cnt_incr : unsigned(10 downto 0);
 begin
 
 data_mem <= data_from_spi;
 address  <= std_logic_vector(word_cnt(DATA_MEM_WIDTH - 2 downto 0));
+word_cnt_incr <= word_cnt + 1;
 	
-combi : process(state, word_cnt, spi_finished)
+combi : process(state, word_cnt, spi_finished, word_cnt_incr)
 begin
-	data_to_spi <= x"000000" & "00000010";
+	data_to_spi <= (others => '0');
 	spi_settings <= '0';
 	spi_start <= '0';
 	we <= '0';
@@ -52,39 +58,53 @@ begin
 	spi_clear <= '0';
 	case (state) is
 		when STARTUP_WAIT =>
-			n_word_cnt <= word_cnt + 1;
+			n_word_cnt <= word_cnt_incr;
 			if(word_cnt = "1111111111") then
-				n_state <= WRITE_SETTINGS_1;
+				n_state <= READ_STATUS_1;
 			end if;
-		when WRITE_SETTINGS_1 =>
+		when READ_STATUS_1 =>
 			n_word_cnt <= (others => '0');
+			data_to_spi <= read_status_settings;
 			spi_settings <= '1';
-			n_state <= WRITE_INSTR_ADDRESS_START;
-		when WRITE_INSTR_ADDRESS_START =>
-			data_to_spi <= x"00" & "00000011" & x"0000";
+			n_state <= READ_STATUS_2;
+		when READ_STATUS_2 =>
+			data_to_spi <= read_status_seq;
 			spi_start <= '1';
-			n_state <= WRITE_INSTR_ADDRESS_WAIT;
-		when WRITE_INSTR_ADDRESS_WAIT =>
+			n_state <= READ_STATUS_3;
+		when READ_STATUS_3 =>
 			if(spi_finished = '1') then
-				n_state <= WRITE_SETTINGS_2;
+				spi_clear <= '1';
+				if(data_from_spi(0) = '0') then
+					n_state <= READ_DATA_1;
+				else
+					n_state <= STARTUP_WAIT;
+				end if;
 			end if;
-		when WRITE_SETTINGS_2 =>
-			data_to_spi <= x"000000" & "00000011";
+		when READ_DATA_1 =>
+			data_to_spi <= read_data_settings_1;
 			spi_settings <= '1';
-			n_state <= RECV_DATA_START;
-		when RECV_DATA_START =>
+			n_state <= READ_DATA_2;
+		when READ_DATA_2 =>
+			data_to_spi <= read_data_seq;
 			spi_start <= '1';
-			n_state <= RECV_DATA_WAIT;
-		when RECV_DATA_WAIT =>
+			n_state <= READ_DATA_3;
+		when READ_DATA_3 =>
 			if(spi_finished = '1') then
-				n_word_cnt <= word_cnt + 1;
+				spi_start <= '1';
+				data_to_spi <= read_data_settings_2;
+				spi_settings <= '1';
+				n_state <= READ_DATA_4;
+			end if;
+		when READ_DATA_4 =>
+			if(spi_finished = '1') then
+				n_word_cnt <= word_cnt_incr;
 				we <= '1';
-				if(word_cnt = "1111111111") then 
+				if(word_cnt = "11111111111") then
 					n_state <= IDLE;
 					spi_clear <= '1';
 				else
 					spi_start <= '1';
-				end if; 
+				end if;
 			end if;
 		when IDLE =>
 			done <= '1';
