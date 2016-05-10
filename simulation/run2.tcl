b34d11df6c8d0fdae44bd9aeea5a212e7d696fed
aasmundo <aaskvoma@hotmail.com>
2016-05-10 16:09:48 +0200

    change from alu to soc simulation

--- simulation/run2.tcl
+++ simulation/run2.tcl
@@ -1,3 +1,3 @@
 database -open -shm -into ncsim.shm waves -default
-probe -create -shm :A :B :operation :result -waveform
+probe -create -shm :clk :nreset :sclk :mosi :miso :pass :fail :cs1 :cs2 :cs3 :cs3 :cs4  -waveform
 run 
--- simulation/runirun.sh
+++ simulation/runirun.sh
@@ -1,3 +1,3 @@
 rm -r INCA_libs/
 irun -clean -compile -mess /eda/tools/cadence/incisive.15.10.008/tools.lnx86/affirma_ams/etc/vhdlams_connectlib_samples/*.vhms
-irun -ultrasim_args +mt=12 -amsfastspice -access +r -v93 -gui -top alu_testbench -mess ../modules/constants.vhdl alu_testbench.vhd /eda/tools/cadence/incisive.15.10.008/tools.lnx86/affirma_ams/etc/vhdlams_connectlib_samples/*.vhms amsd.scs -input run2.tcl -run | tee deleteme.tmp
+irun -ultrasim_args +mt=12 -amsfastspice -access +r -v93 -gui -top soc_top_tb -mess ../modules/constants.vhdl ../modules/soc_top_tb.vhd /eda/tools/cadence/incisive.15.10.008/tools.lnx86/affirma_ams/etc/vhdlams_connectlib_samples/*.vhms amsd.scs -input run2.tcl -run | tee deleteme.tmp
