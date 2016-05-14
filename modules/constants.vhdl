e213a989d9c010749662e967a3557d10f405c620
Aasmund <aaskvoma@hotmail.com>
2016-05-14 14:37:15 +0200

    set prediction table size to 32 (2^5)

--- modules/constants.vhdl
+++ modules/constants.vhdl
@@ -76,7 +76,7 @@ package constants is
 	constant PC_WIDTH : integer := 11;
 	
 	--branch prediction
-	constant PREDICTION_TABLE_SIZE : integer :=8;
+	constant PREDICTION_TABLE_SIZE : integer :=5;
 
 	
 
